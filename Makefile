9155f78a6ae629b662124ae3b15e4be1b411c1c7
Darren Embry <dse@webonastick.com>
2022-09-24 02:52:04 -0400

    enhancements

---
+++ Makefile
@@ -0,0 +1,9 @@
+default:
+	./nokiatune.sh
+	./nokiatune.sh 2
+	./nokiatune.sh 5
+	./nokiatune.sh 10
+	./nokiatune.sh trapezium
+	./nokiatune.sh trapezium 2
+	./nokiatune.sh trapezium 5
+	./nokiatune.sh trapezium 10
--- nokiatune.sh
+++ nokiatune.sh
@@ -2,7 +2,27 @@
 #
 # I'll make this easier eventually.
 
+# type is one of sine, square, triangle, sawtooth, trapezium, exp,
+# [white]noise, tpdfnoise, pinknoise, brownnoise, pluck; default=sine.
 type=exp
+gain=0
+
+typex=""
+gainx=""
+
+for i ; do
+    case "$i" in
+        sine|square|triangle|sawtooth|trapezium|exp|pluck)
+            type="$i"
+            typex="-$i"
+            ;;
+        [0-9]*)
+            gain="-$i"
+            gainx="-$i"
+            ;;
+    esac
+done
+
 eighth=0.125
 delay=0.25
 quarter="$(echo $eighth \* 2 | bc -l)"
@@ -13,25 +33,31 @@ remainder="$(echo 6 - $eighth \* 24 - $delay | bc -l)"
 tmpdir="$(mktemp -d /tmp/nokiatune.XXXXXXXXXX)"
 
 sox -q -n $tmpdir/00.wav synth $delay         $type A5  gain -1000
-sox -q -n $tmpdir/01.wav synth $eighth        $type E6  gain -2
-sox -q -n $tmpdir/02.wav synth $eighth        $type D6  gain -2
-sox -q -n $tmpdir/03.wav synth $quarter       $type F#5 gain -2
-sox -q -n $tmpdir/04.wav synth $quarter       $type G#5 gain -2
-sox -q -n $tmpdir/05.wav synth $eighth        $type C#6 gain -2
-sox -q -n $tmpdir/06.wav synth $eighth        $type B5  gain -2
-sox -q -n $tmpdir/07.wav synth $quarter       $type D5  gain -2
-sox -q -n $tmpdir/08.wav synth $quarter       $type E5  gain -2
-sox -q -n $tmpdir/09.wav synth $eighth        $type B5  gain -2
-sox -q -n $tmpdir/10.wav synth $eighth        $type A5  gain -2
-sox -q -n $tmpdir/11.wav synth $quarter       $type C#5 gain -2
-sox -q -n $tmpdir/12.wav synth $quarter       $type E5  gain -2
-sox -q -n $tmpdir/13.wav synth $threequarters $type A5  gain -2
+sox -q -n $tmpdir/01.wav synth $eighth        $type E6  gain "${gain}"
+sox -q -n $tmpdir/02.wav synth $eighth        $type D6  gain "${gain}"
+sox -q -n $tmpdir/03.wav synth $quarter       $type F#5 gain "${gain}"
+sox -q -n $tmpdir/04.wav synth $quarter       $type G#5 gain "${gain}"
+sox -q -n $tmpdir/05.wav synth $eighth        $type C#6 gain "${gain}"
+sox -q -n $tmpdir/06.wav synth $eighth        $type B5  gain "${gain}"
+sox -q -n $tmpdir/07.wav synth $quarter       $type D5  gain "${gain}"
+sox -q -n $tmpdir/08.wav synth $quarter       $type E5  gain "${gain}"
+sox -q -n $tmpdir/09.wav synth $eighth        $type B5  gain "${gain}"
+sox -q -n $tmpdir/10.wav synth $eighth        $type A5  gain "${gain}"
+sox -q -n $tmpdir/11.wav synth $quarter       $type C#5 gain "${gain}"
+sox -q -n $tmpdir/12.wav synth $quarter       $type E5  gain "${gain}"
+sox -q -n $tmpdir/13.wav synth $threequarters $type A5  gain "${gain}"
 sox -q -n $tmpdir/14.wav synth $remainder     $type A5  gain -1000
 
-sox $tmpdir/*.wav nokiatune.wav
-sox $tmpdir/*.wav nokiatune.mp3
+dir="sounds"
+basename="nokiatune${typex}${gainx}"
 
-ffmpeg -i nokiatune.wav nokiatune.m4a
-cp nokiatune.m4a nokiatune.m4r
+mkdir -p "${dir}"
+
+sox $tmpdir/*.wav "${dir}/${basename}".wav
+
+ffmpeg -y -i "${dir}/${basename}".wav "${dir}/${basename}".mp3
+ffmpeg -y -i "${dir}/${basename}".wav "${dir}/${basename}".m4a
+ffmpeg -y -i "${dir}/${basename}".wav "${dir}/${basename}".flac
+cp "${dir}/${basename}".m4a "${dir}/${basename}".m4r
 
 /bin/rm -f -r $tmpdir
